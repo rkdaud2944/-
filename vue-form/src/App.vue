12b73f509f9304bb7649a3c20e6079079d320ede
DESKTOP-SNTE4GG\rkdau <rkdaud2944@naver.com>
2023-01-25 04:30:53 +0900

    중간

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,14 +1,70 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-      <div>
-a sd
+
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>  {{data[push].title}}</h4>
+      <p>내용</p>
+      <button @click="modal = false"> 닫기 </button>
+    </div>
+
+  </div>
+
+
+
+  <p class="title">코끼리 커뮤니케이션</p>
+    <div class="menu">
+        <a v-for="a in menus" :key="a"> {{ a }}</a>
+    </div>
+
+      <div v-for="(b,i) in data" :key="i">
+        <img :src="b.img" class="contents-img" @click="modal = true ; push = i ">
+        <h4> {{b.title}} </h4>
+        <p>{{b.price}}만원</p>
+        <button @click="recommend[i]++">추천버튼</button>
+        <span>추천수 : {{recommend[i]}}</span>
       </div>
+      <!-- <div>
+        <img src="./assets/image2.jpg" class="contents-img" @click="modal = true">
+        <h4> {{data[1].title}} </h4>
+        <p>{{data[1].price}}만원</p>
+        <button @click="recommend[1]++">추천버튼</button>
+        <span>추천수 : {{recommend[1]}}</span>
+      </div>
+      <div>
+        <img src="./assets/image1.jpg" class="contents-img" @click="modal = true">
+        <h4> {{data[2].title}} </h4>
+        <p>{{data[2].price}}만원</p>
+        <button @click="recommend[2]++">추천버튼</button>
+        <span>추천수 : {{recommend[2]}}</span>
+      </div> -->
+
 </template>
 
 <script>
 
+import data1 from './assets/data.js';
+
 export default {
   name: 'App',
+  data(){
+    return {
+      push : 0,
+      data : data1,
+      modal :  false,
+      price : ['10','1','100'],
+      recommend : ['0','0','0'],
+      menus : ['코끼리 상품', '진짜 코끼리', 'QnA'],
+      products : ['코끼리 봇','코끼리 용품','아프리카 코끼리'],
+      images : ['image3.jpg','image2.jpg','image1.jpg']
+    }
+  }, 
+  
+  methods : {
+    report(){
+      this.Howmuchreport[1] ++ ;
+    }
+  },
+
   components: {
   }
 }
@@ -21,6 +77,46 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 30px;
+}
+
+.menu {
+  background: grey;
+  padding:  15px;
+  border-radius: 5px;
   margin-top: 60px;
+}
+
+.menu a {
+  color: white;
+  padding: 100px;
+}
+
+.title {
+  font-size: 30px;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.contents-img {
+  width: 30%;
+  margin-top: 40px;
+}
+
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 5px;
+  padding: 20px;
 }
 </style>
---
+++ vue-form/src/assets/data.js
@@ -0,0 +1,23 @@
+
+export default [{
+    id : 0,
+    title: "코끼리 봇",
+    img : "https://codingapple1.github.io/vue/room1.jpg",
+    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+    price: 10
+    },
+    {
+    id : 1,
+    title: "코끼리 용품",
+    img : "image2.jpg",
+    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+    price: 1
+    },
+    {
+    id : 2,
+    title: "진짜 코끼리",
+    img : "image1.jpg",
+    image: "https://codingapple1.github.io/vue/room2.jpg",
+    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
+    price: 300
+    }];
